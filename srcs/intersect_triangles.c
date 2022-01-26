705cf5b021b8217e0347f135da49a55807910357
Hugothms <hugothms@gmail.com>
2022-01-26 22:23:17 +0100

    nope

--- srcs/intersect_triangles.c
+++ srcs/intersect_triangles.c
@@ -6,88 +6,127 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 17:16:38 by hthomas           #+#    #+#             */
-/*   Updated: 2022/01/26 13:41:30 by hthomas          ###   ########.fr       */
+/*   Updated: 2022/01/26 22:23:12 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
+// bool	intersect_triangle(const t_ray ray, const t_triangle triangle, t_impact *impact)
+// {
+// 	(void)impact;
+// 	t_vect v0v1 = sub_vect(triangle.v1, triangle.v0);
+// 	t_vect v0v2 = sub_vect(triangle.v2, triangle.v0);
+// 	t_vect pvec = cross_product(ray.dir, v0v2);
+// 	double det = dot_product(v0v1, pvec);
+// 	if (fabs(det) < EPSILON)
+// 		return 0;
+// 	double invDet = 1 / det;
+
+// 	t_vect tvec = sub_vect(ray.pos, triangle.v0);
+// 	double u = dot_product(tvec, pvec) * invDet;
+// 	if (u < 0 || u > 1)
+// 		return 0;
+
+// 	t_vect qvec = cross_product(tvec, v0v1);
+// 	double v = dot_product(ray.dir, qvec) * invDet;
+// 	if (v < 0 || u + v > 1)
+// 		return 0;
+
+// 	double t = dot_product(v0v2, qvec) * invDet;
+// 	(void)t;
+// 	return 1;
+// 	/*
+// 	// compute plane's normal
+// 	Vec3f v0v1 = v1 - v0;
+// 	Vec3f v0v2 = v2 - v0;
+// 	// no need to normalize
+// 	Vec3f N = cross_product(v0v1, v0v2); // N
+// 	double denom = dot_product(N, N);
+
+// 	// Step 1: finding P
+
+// 	// check if ray and plane are parallel ?
+// 	double NdotRayray.Direction = dot_product(N, ray.dir);
+// 	if (fabs(NdotRayray.Direction) < EPSILON) // almost 0
+// 		return false; // they are parallel so they don't intersect !
+
+// 	// compute d parameter using equation 2
+// 	double d = dot_product(N, v0);
+
+// 	// compute t (equation 3)
+// 	t = (dot_product(N, ray.pos) + d) / NdotRayray.Direction;
+// 	// check if the triangle is in behind the ray
+// 	if (t < 0) return false; // the triangle is behind
+
+// 	// compute the intersection point using equation 1
+// 	Vec3f P = ray.pos + t * ray.dir;
+
+// 	// Step 2: inside-outside test
+// 	Vec3f C; // vector perpendicular to triangle's plane
+
+// 	// edge 0
+// 	Vec3f edge0 = v1 - v0;
+// 	Vec3f vp0 = P - v0;
+// 	C = cross_product(edge0, vp0);
+// 	if (dot_product(N, C) < 0) return false; // P is on the right side
+
+// 	// edge 1
+// 	Vec3f edge1 = v2 - v1;
+// 	Vec3f vp1 = P - v1;
+// 	C = cross_product(edge1, vp1);
+// 	if ((u = dot_product(N, C)) < 0)  return false; // P is on the right side
+
+// 	// edge 2
+// 	Vec3f edge2 = v0 - v2;
+// 	Vec3f vp2 = P - v2;
+// 	C = cross_product(edge2, vp2);
+// 	if ((v = dot_product(N, C)) < 0) return false; // P is on the right side;
+
+// 	u /= denom;
+// 	v /= denom;
+
+// 	return true; // this ray hits the triangle
+
+// }
+
 bool	intersect_triangle(const t_ray ray, const t_triangle triangle, t_impact *impact)
 {
-	(void)impact;
-	t_vect v0v1 = sub_vect(triangle.v1, triangle.v0);
-	t_vect v0v2 = sub_vect(triangle.v2, triangle.v0);
-	t_vect pvec = cross_product(ray.dir, v0v2);
-	double det = dot_product(v0v1, pvec);
-	if (fabs(det) < EPSILON)
-		return 0;
-	double invDet = 1 / det;
+	t_vect v0v1;
+	t_vect v0v2;
+	t_vect pvec;
+	t_vect tvec;
+	t_vect qvec;
+	float det;
+	float inv_det;
+	float u;
+	float v;
+	float t;
 
-	t_vect tvec = sub_vect(ray.pos, triangle.v0);
-	double u = dot_product(tvec, pvec) * invDet;
-	if (u < 0 || u > 1)
-		return 0;
-
-	t_vect qvec = cross_product(tvec, v0v1);
-	double v = dot_product(ray.dir, qvec) * invDet;
-	if (v < 0 || u + v > 1)
-		return 0;
-
-	double t = dot_product(v0v2, qvec) * invDet;
-	(void)t;
-	return 1;
-	/*
-	// compute plane's normal
-	Vec3f v0v1 = v1 - v0;
-	Vec3f v0v2 = v2 - v0;
-	// no need to normalize
-	Vec3f N = cross_product(v0v1, v0v2); // N
-	double denom = dot_product(N, N);
-
-	// Step 1: finding P
-
-	// check if ray and plane are parallel ?
-	double NdotRayray.Direction = dot_product(N, ray.dir);
-	if (fabs(NdotRayray.Direction) < EPSILON) // almost 0
-		return false; // they are parallel so they don't intersect !
-
-	// compute d parameter using equation 2
-	double d = dot_product(N, v0);
-
-	// compute t (equation 3)
-	t = (dot_product(N, ray.pos) + d) / NdotRayray.Direction;
-	// check if the triangle is in behind the ray
-	if (t < 0) return false; // the triangle is behind
-
-	// compute the intersection point using equation 1
-	Vec3f P = ray.pos + t * ray.dir;
-
-	// Step 2: inside-outside test
-	Vec3f C; // vector perpendicular to triangle's plane
-
-	// edge 0
-	Vec3f edge0 = v1 - v0;
-	Vec3f vp0 = P - v0;
-	C = cross_product(edge0, vp0);
-	if (dot_product(N, C) < 0) return false; // P is on the right side
-
-	// edge 1
-	Vec3f edge1 = v2 - v1;
-	Vec3f vp1 = P - v1;
-	C = cross_product(edge1, vp1);
-	if ((u = dot_product(N, C)) < 0)  return false; // P is on the right side
-
-	// edge 2
-	Vec3f edge2 = v0 - v2;
-	Vec3f vp2 = P - v2;
-	C = cross_product(edge2, vp2);
-	if ((v = dot_product(N, C)) < 0) return false; // P is on the right side;
-
-	u /= denom;
-	v /= denom;
-
-	return true; // this ray hits the triangle
-*/
+	v0v1 = sub_vect(triangle.v1, triangle.v0);
+	v0v2 = sub_vect(triangle.v2, triangle.v0);
+	pvec = cross_product(ray.dir, v0v2);
+	det = dot_product(v0v1, pvec);
+	if (det < EPSILON)
+		return (false);
+	inv_det = 1.0 / det;
+	tvec = sub_vect(ray.pos, triangle.v0);
+	u = dot_product(tvec, pvec) * inv_det;
+	if (u < 0.0 || u > 1.0)
+		return (false);
+	qvec = cross_product(tvec, v0v1);
+	v = dot_product(ray.dir, qvec) * inv_det;
+	if (v < 0.0 || u + v > 1.0)
+		return (false);
+	t = dot_product(v0v2, qvec) * inv_det;
+	if (t > EPSILON)
+	{
+		impact->dist = t;
+		impact->pos = add_vect(ray.pos, multi_vect(ray.dir, t));
+		impact->normal = normalize(cross_product(v0v1, v0v2));
+		return (true);
+	}
+	return (false);
 }
 
 void	ray_triangles(const t_ray ray, const t_scene *scene, t_impact *impact, void **object)
